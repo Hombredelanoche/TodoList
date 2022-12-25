183319d1d052e7a2e6b76a9cf3248f0241115157
Elhombredelanoche <elbrandonpro@yahoo.fr>
2022-12-25 04:45:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function createTask() {
+  const task = document.querySelector("#task").value;
+  if (task === "") {
+    console.log("Something didn't worked");
+  } else {
+    document.querySelector(
+      ".remainingTask"
+    ).innerHTML += `<article class='taskContainer'><div class='newTask'> ${task} </div> <button class='deleteTask' onclick='deleteTask()'><i class="fa-solid fa-trash"></i></button></article>`;
+    task.value === "";
+  }
+}
+
+function deleteTask() {
+  document.querySelector(".taskContainer").innerHTML = "";
+}
